0c9157b00afd24f61808afb6564bf3e9aede2f21
wryl <wryl@163.com>
2021-03-09 15:11:34 +0800

    bugfixed

--- Server/Hotfix/mahjong/Handler/SessionPlayerComponentSystem.cs
+++ Server/Hotfix/mahjong/Handler/SessionPlayerComponentSystem.cs
@@ -7,7 +7,11 @@ namespace ET
 		public override void Destroy(SessionPlayerComponent self)
 		{
 			// 发送断线消息
-			ActorLocationSenderComponent.Instance.Send(self.Player.UnitId, new G2M_SessionDisconnect());
+			if (self.Player.UnitId != 0)
+			{
+				// 发送断线消息
+				ActorLocationSenderComponent.Instance.Send(self.Player.UnitId, new G2M_SessionDisconnect());
+			}
 			Game.Scene.GetComponent<PlayerComponent>()?.Remove(self.Player.Id);
 		}
 	}
